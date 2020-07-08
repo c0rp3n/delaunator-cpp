be748e0a6ba20fae6d0175a4e20f2bff14bc31ff
c0rp3n <ojhitchcock@gmail.com>
2020-07-08 15:56:49 +0100

    Add single header option

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -2,4 +2,5 @@ option(WITH_TESTS
     "Choose if delaunator tests should be built" TRUE)
 add_feature_info("Unit tests" WITH_TESTS "Delaunator unit tests")
 
+option(DELAUNATOR_SINGLE_HEADER "Whether to the library should be header only." OFF)
 option(DELAUNATOR_SINGLE_PRECISION "Whether to use single precision floating point values." OFF)
